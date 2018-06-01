964c7dfe6acdc928573783e79ae9f7168fbbb90b
l.aminov <l.aminov@tinkoff.ru>
2018-06-01 09:38:20 +0500

    Update for 3.7 support

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# Just another Prometheus exporter
+# Pgpool2 Prometheus Exporter
+
+## Arguments
+
+* `web.telemetry-path` – Path under which to expose metrics
+* `web.listen-address` – Address on which to expose metrics and web interface
+* `pcp.passfile` – Path to the PCP password file containing hostname:port:username:password
+* `pcp.host` – PCP hostname
+* `pcp.port` – PCP port
+* `pcp.username` – PCP username
+* `pcp.password` – PCP password
 
 ## Metrics
 
@@ -9,3 +19,8 @@
 * `pgpool2_proc_count`
 * `pgpool2_frontend_active_connections`
 * `pgpool2_frontend_inactive_connections`
+* `pgpool2_watchdog_nodes_total`
+* `pgpool2_watchdog_nodes_remote`
+* `pgpool2_watchdog_nodes_alive_remote`
+* `pgpool2_watchdog_vip`
+* `pgpool2_watchdog_quorum_state`
--- exporter.go
+++ exporter.go
@@ -4,6 +4,9 @@ import (
 	"strconv"
 	"time"
 
+	"fmt"
+
+	"github.com/leominov/pgpool2-exporter/pgpool2"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/version"
 	"github.com/sirupsen/logrus"
@@ -33,7 +36,7 @@ var (
 	PoolNodeInfo = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "node_info"),
 		"Displays the information of node",
-		[]string{"id", "node", "port", "width"}, nil,
+		[]string{"id", "node", "port", "width", "role"}, nil,
 	)
 	PoolProcCount = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, "", "proc_count"),
@@ -50,24 +53,158 @@ var (
 		"Displays number of all inactive connections to all Pgpool-II children processes",
 		[]string{"database"}, nil,
 	)
+	WatchdogTotalNodes = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "watchdog", "nodes_total"),
+		"Watchdog total nodes",
+		nil, nil,
+	)
+	WatchdogRemoteNodes = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "watchdog", "nodes_remote"),
+		"Watchdog remote nodes",
+		nil, nil,
+	)
+	WatchdogAliveRemoteNodes = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "watchdog", "nodes_alive_remote"),
+		"Watchdog alive remote nodes",
+		nil, nil,
+	)
+	WatchdogVIP = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "watchdog", "vip"),
+		"Watchdog virtual IP",
+		nil, nil,
+	)
+	WatchdogQuorumState = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "watchdog", "quorum_state"),
+		"Watchdog quorum state (1 is ok)",
+		nil, nil,
+	)
 )
 
 type Exporter struct {
-	pgpool *PGPoolClient
+	pgpool *pgpool2.Client
 }
 
 func init() {
 	prometheus.MustRegister(version.NewCollector(exporterName))
 }
 
-func NewExporter(pgpool *PGPoolClient) *Exporter {
+func NewExporter(pgpool *pgpool2.Client) *Exporter {
 	return &Exporter{
 		pgpool: pgpool,
 	}
 }
 
+func (e *Exporter) collectNodeMetrics(ch chan<- prometheus.Metric) error {
+	nodeCount, err := e.pgpool.ExecNodeCount()
+	if err != nil {
+		return fmt.Errorf("ExecNodeCount() error: %v", err)
+	}
+	ch <- prometheus.MustNewConstMetric(
+		PoolNodeCount,
+		prometheus.GaugeValue,
+		float64(nodeCount),
+	)
+	for i := 0; i < nodeCount; i++ {
+		nodeInfo, err := e.pgpool.ExecNodeInfo(i)
+		if err != nil {
+			return fmt.Errorf("ExecNodeInfo(%d) error: %v", i, err)
+		}
+		ch <- prometheus.MustNewConstMetric(
+			PoolNodeInfo,
+			prometheus.GaugeValue,
+			float64(nodeInfo.StatusCode),
+			strconv.Itoa(i),
+			nodeInfo.Hostname,
+			strconv.Itoa(nodeInfo.Port),
+			strconv.FormatFloat(nodeInfo.Weight, 'f', 6, 64),
+			nodeInfo.Role,
+		)
+	}
+	return nil
+}
+
+func (e *Exporter) collectProcCountMetrics(ch chan<- prometheus.Metric) error {
+	procArr, err := e.pgpool.ExecProcCount()
+	if err != nil {
+		return fmt.Errorf("ExecProcCount() error: %v", err)
+	}
+	ch <- prometheus.MustNewConstMetric(
+		PoolProcCount,
+		prometheus.GaugeValue,
+		float64(len(procArr)),
+	)
+	return nil
+}
+
+func (e *Exporter) collectProcInfoMetrics(ch chan<- prometheus.Metric) error {
+	procInfoArr, err := e.pgpool.ExecProcInfo()
+	if err != nil {
+		return fmt.Errorf("ExecProcInfo() error: %v", err)
+	}
+	procSummary := e.pgpool.ProcInfoSummary(procInfoArr)
+	for database, counter := range procSummary.Active {
+		ch <- prometheus.MustNewConstMetric(
+			PoolNumberActiveConnections,
+			prometheus.GaugeValue,
+			float64(counter),
+			database,
+		)
+	}
+	for database, counter := range procSummary.Inactive {
+		ch <- prometheus.MustNewConstMetric(
+			PoolNumberInactiveConnections,
+			prometheus.GaugeValue,
+			float64(counter),
+			database,
+		)
+	}
+	return nil
+}
+
+func (e *Exporter) collectWatchdogInfoMetrics(ch chan<- prometheus.Metric) error {
+	watchdogInfo, err := e.pgpool.ExecWatchdogInfo()
+	if err != nil {
+		return fmt.Errorf("ExecWatchdogInfo() error: %v", err)
+	}
+	ch <- prometheus.MustNewConstMetric(
+		WatchdogTotalNodes,
+		prometheus.GaugeValue,
+		float64(watchdogInfo.TotalNodes),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		WatchdogRemoteNodes,
+		prometheus.GaugeValue,
+		float64(watchdogInfo.RemoteNodes),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		WatchdogAliveRemoteNodes,
+		prometheus.GaugeValue,
+		float64(watchdogInfo.AliveRemoteNodes),
+	)
+	ch <- prometheus.MustNewConstMetric(
+		WatchdogQuorumState,
+		prometheus.GaugeValue,
+		float64(watchdogInfo.QuorumStateCode),
+	)
+	if watchdogInfo.VIP {
+		ch <- prometheus.MustNewConstMetric(
+			WatchdogVIP,
+			prometheus.GaugeValue,
+			1.0,
+		)
+	} else {
+		ch <- prometheus.MustNewConstMetric(
+			WatchdogVIP,
+			prometheus.GaugeValue,
+			0.0,
+		)
+	}
+	return nil
+}
+
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 	var scrapeError bool
+
 	defer func(begun time.Time) {
 		ch <- prometheus.MustNewConstMetric(
 			PoolLastScrapeDuration,
@@ -75,82 +212,32 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 			time.Since(begun).Seconds(),
 		)
 	}(time.Now())
-	nodeCount, err := e.pgpool.ExecNodeCount()
-	if err != nil {
+
+	if err := e.collectNodeMetrics(ch); err != nil {
 		scrapeError = true
-		logrus.Errorf("ExecNodeCount error: %v", err)
-		ch <- prometheus.MustNewConstMetric(
-			PoolNodeCount,
-			prometheus.GaugeValue,
-			0.0,
-		)
-	} else {
-		ch <- prometheus.MustNewConstMetric(
-			PoolNodeCount,
-			prometheus.GaugeValue,
-			float64(nodeCount),
-		)
-		for i := 0; i < nodeCount; i++ {
-			nodeInfo, err := e.pgpool.ExecNodeInfo(i)
-			if err != nil {
-				scrapeError = true
-				logrus.Errorf("ExecNodeInfo error: %v", err)
-				continue
-			}
-			ch <- prometheus.MustNewConstMetric(
-				PoolNodeInfo,
-				prometheus.GaugeValue,
-				float64(nodeInfo.Status),
-				strconv.Itoa(i),
-				nodeInfo.Hostname,
-				strconv.Itoa(nodeInfo.Port),
-				strconv.FormatFloat(nodeInfo.Weight, 'f', 6, 64),
-			)
-		}
+		logrus.Error(err)
 	}
-	procArr, err := e.pgpool.ExecProcCount()
-	if err != nil {
+
+	if err := e.collectProcCountMetrics(ch); err != nil {
 		scrapeError = true
-		logrus.Errorf("ExecProcCount error: %v", err)
-		ch <- prometheus.MustNewConstMetric(
-			PoolProcCount,
-			prometheus.GaugeValue,
-			0.0,
-		)
-	} else {
-		ch <- prometheus.MustNewConstMetric(
-			PoolProcCount,
-			prometheus.GaugeValue,
-			float64(len(procArr)),
-		)
+		logrus.Error(err)
 	}
-	procInfoArr, err := e.pgpool.ExecProcInfo()
-	if err != nil {
+
+	if err := e.collectProcInfoMetrics(ch); err != nil {
 		scrapeError = true
-		logrus.Errorf("ExecProcInfo error: %v", err)
-	} else {
-		procSummary := e.pgpool.ProcInfoSummary(procInfoArr)
-		for database, counter := range procSummary.Active {
-			ch <- prometheus.MustNewConstMetric(
-				PoolNumberActiveConnections,
-				prometheus.GaugeValue,
-				float64(counter),
-				database,
-			)
-		}
-		for database, counter := range procSummary.Inactive {
-			ch <- prometheus.MustNewConstMetric(
-				PoolNumberInactiveConnections,
-				prometheus.GaugeValue,
-				float64(counter),
-				database,
-			)
-		}
+		logrus.Error(err)
 	}
+
+	if err := e.collectWatchdogInfoMetrics(ch); err != nil {
+		scrapeError = true
+		logrus.Error(err)
+	}
+
 	scrapeErrorFloat := 0.0
 	if scrapeError {
 		scrapeErrorFloat = 1.0
 	}
+
 	ch <- prometheus.MustNewConstMetric(
 		PoolLastScrapeError,
 		prometheus.GaugeValue,
@@ -166,4 +253,9 @@ func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 	ch <- PoolNodeInfo
 	ch <- PoolNumberActiveConnections
 	ch <- PoolNumberInactiveConnections
+	ch <- WatchdogTotalNodes
+	ch <- WatchdogRemoteNodes
+	ch <- WatchdogAliveRemoteNodes
+	ch <- WatchdogQuorumState
+	ch <- WatchdogVIP
 }
